729495fcf57d128473a008cd02ee014acb17e172
Kiss Ferenc <kiss.ferenc.1984@gmail.com>
2016-10-16 14:15:28 +0300

    edit remove buttons

--- app/app.js
+++ app/app.js
@@ -4,6 +4,26 @@ import ReactDOM from 'react-dom';
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Comment = React.createClass({
+        edit: function() {
+            alert('Editing comment');
+        },
+        
+        remove: function () {
+            alert("Removing comment");
+        },
+        
+        render: function () {
+            return (
+                <div className="commentContainer">
+                    <div className="commentText">{this.props.children}</div>
+                    <button onClick={this.edit} className="button-primary">Edit</button>
+                    <button onClick={this.remove} className="button-danger">Remove</button>
+                </div>
+            );
+        }
+    });
+
 var Component = React.createClass({
   render: function() {
     return (
@@ -15,10 +35,12 @@ var Component = React.createClass({
 
 ReactDOM.render(
     <div>
-        <Component text="Component text1" />
-        <Component text="Component text2" />
-        <Component text="Component text3" />
+        <Comment>Hey my name is Fercsi</Comment>
+        <Comment>Beans</Comment>
+        <Comment>Tuna</Comment>
     </div>,
+
+
     document.getElementById('main')
 );
     
