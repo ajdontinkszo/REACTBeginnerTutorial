184f838dd5a61750a1d0fef57cd825e46e582009
Kiss Ferenc <kiss.ferenc.1984@gmail.com>
2016-10-17 13:41:53 +0300

    edit-save OK, remove from board OK

--- app/app.js
+++ app/app.js
@@ -40,26 +40,26 @@ var ReactDOM = require('react-dom');
 
 var Comment = React.createClass({
     getInitialState: function () {
-        return {editing: false}
+        return { editing: false }
     },
 
     edit: function () {
         /*alert('Editing comment');*/
-        this.setState({editing: true});
+        this.setState({ editing: true });
     },
 
     remove: function () {
-        alert("Removing comment");
+        console.log("Removing comment");
+        this.props.deleteFromBoard(this.props.index);
     },
 
     save: function () {
-        var val = this.refs.newText.value;
-        console.log("new comment: " + val);
-        this.setState({editing: false});
+        this.props.updateCommentText(this.refs.newText.value, this.props.index)        
+        this.setState({ editing: false });
     },
 
-    renderNormal: function(){
-         return (
+    renderNormal: function () {
+        return (
             <div className="commentContainer">
                 <div className="commentText">{this.props.children}</div>
                 <button onClick={this.edit} className="button-primary">Edit</button>
@@ -68,8 +68,8 @@ var Comment = React.createClass({
         );
     },
 
-    renderForm: function() {
-         return (
+    renderForm: function () {
+        return (
             <div className="commentContainer">
                 <textarea ref="newText" defaultValue={this.props.children}></textarea>
                 <button onClick={this.save} className="button-success">Save</button>
@@ -78,34 +78,53 @@ var Comment = React.createClass({
     },
 
     render: function () {
-       if(this.state.editing) {
-           return this.renderForm();
-       } else  {
-           return this.renderNormal();
-       }
+        if (this.state.editing) {
+            return this.renderForm();
+        } else {
+            return this.renderNormal();
+        }
     }
 });
 
 var Board = React.createClass({
-    
-    getInitialState: function() {
+
+    getInitialState: function () {
         return {
             comments: [
                 'I like bacon',
                 'Want to get ice cream?',
-                'Ok, we have enough comments now'
+                'Ok, we have enough comments now',
+                'alma a  fa  alatt'
             ]
         };
     },
 
-    render: function() {
-        return(
+    removeComment: function(i) {
+        console.log('Removing comments: ' + i);
+        var arr = this.state.comments;
+        arr.splice(i,1);
+        this.setState({comments: arr})
+    },
+
+    updateComment: function(newText, i) {
+        console.log('Updating comment');
+        var arr = this.state.comments;
+        arr[i] = newText;
+        this.setState({comments: arr});
+    },
+ 
+    eachComment: function (text, i) {
+        return (
+            <Comment key={i} index={i} updateCommentText={this.updateComment} deleteFromBoard={this.removeComment}>
+                {text}
+            </Comment>
+        );
+    },
+
+    render: function () {
+        return (
             <div className="board">
-                {
-                    this.state.comments.map(function(text, index) {
-                        return(<Comment key={index}>{text}</Comment>);
-                    })
-                }
+                {this.state.comments.map(this.eachComment)}
             </div>
         );
     }
