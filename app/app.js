5443fe377ab471ca48368a62a7ac9c3258c88d9d
Kiss Ferenc <kiss.ferenc.1984@gmail.com>
2016-10-17 11:31:41 +0300

    edit - save button

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ var ReactDOM = require('react-dom');
 
 
 
-var CheckBox = React.createClass({
+/*var CheckBox = React.createClass({
        
 
     getInitialState: function() {
@@ -36,16 +36,71 @@ var CheckBox = React.createClass({
           </div>
       );
       
-  }
+  }*/
 
-  
+var Comment = React.createClass({
+    getInitialState: function () {
+        return {editing: false}
+    },
+
+    edit: function () {
+        /*alert('Editing comment');*/
+        this.setState({editing: true});
+    },
+
+    remove: function () {
+        alert("Removing comment");
+    },
+
+    save: function () {
+        /*alert('Editing comment');*/
+        this.setState({editing: false});
+    },
+
+    renderNormal: function(){
+         return (
+            <div className="commentContainer">
+                <div className="commentText">{this.props.children}</div>
+                <button onClick={this.edit} className="button-primary">Edit</button>
+                <button onClick={this.remove} className="button-danger">Remove</button>
+            </div>
+        );
+    },
+
+    renderForm: function() {
+         return (
+            <div className="commentContainer">
+                <textarea className="commentText">{this.props.children}</textarea>
+                <button onClick={this.save} className="button-success">Save</button>
+            </div>
+        );
+    },
+
+    render: function () {
+       if(this.state.editing) {
+           return this.renderForm();
+       } else  {
+           return this.renderNormal();
+       }
+    }
 });
 
+
+
 ReactDOM.render(
+    <div>
+        <Comment>Hey my name is Fercsi</Comment>
+        <Comment>Beans</Comment>
+        <Comment>Tuna</Comment>
+    </div>,
+    document.getElementById('main')
+);
+
+/*ReactDOM.render(
     <CheckBox />,
     document.getElementById('main')
 );
-    
+    */
 
 
 
